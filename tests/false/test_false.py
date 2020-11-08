7a462d2b14425e6c05dee95d604dbdc480158995
duyixian1234 <duyixian1234@qq.com>
2020-11-08 14:59:00 +0000

    `false ` is ready

---
+++ tests/false/test_false.py
@@ -0,0 +1,8 @@
+from click.testing import CliRunner
+from u_coreutils.false import false
+
+
+def test_false():
+    runner = CliRunner()
+    result = runner.invoke(false)
+    assert result.exit_code == 1
